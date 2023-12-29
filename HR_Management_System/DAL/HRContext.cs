fee2ff62620e5d2cce286a04cf0b5821ae8ae9b8
SyMihad <symihad@gmail.com>
2023-12-30 05:04:25 +0600

    Database creation and connection tested

---
+++ HR_Management_System/DAL/HRContext.cs
@@ -0,0 +1,15 @@
+﻿using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    internal class HRContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
